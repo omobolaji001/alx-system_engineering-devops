f6244ada919cc91f98ceb0cd12757751b734457a
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-12-23 08:19:22 +0100

    modifications

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # Install an especific version of flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
+python_packages = ['python 3.8.10', 'flask 2.1.0', 'werkzeug 2.1.1']
+
+package { $python_packages:
+  ensure   => 'installed',
   provider => 'pip3',
 }
